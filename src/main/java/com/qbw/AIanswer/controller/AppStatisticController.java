5b24e098358138ec4af58e1c4f7f83ad6bd99d0e
bushiqbw <1036279449@qq.com>
2024-08-11 21:31:33 +0800

    使用原生Mybatis实现统计分析功能

---
+++ src/main/java/com/qbw/AIanswer/controller/AppStatisticController.java
@@ -0,0 +1,53 @@
+package com.qbw.AIanswer.controller;
+
+import com.qbw.AIanswer.common.BaseResponse;
+import com.qbw.AIanswer.common.ErrorCode;
+import com.qbw.AIanswer.common.ResultUtils;
+import com.qbw.AIanswer.exception.ThrowUtils;
+import com.qbw.AIanswer.mapper.UserAnswerMapper;
+import com.qbw.AIanswer.model.dto.statistic.AppAnswerCountDTO;
+import com.qbw.AIanswer.model.dto.statistic.AppAnswerResultCountDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * App 统计分析接口
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@RestController
+@RequestMapping("/app/statistic")
+@Slf4j
+public class AppStatisticController {
+
+    @Resource
+    private UserAnswerMapper userAnswerMapper;
+
+    /**
+     * 热门应用及回答数统计（top 10）
+     *
+     * @return
+     */
+    @GetMapping("/answer_count")
+    public BaseResponse<List<AppAnswerCountDTO>> getAppAnswerCount() {
+        return ResultUtils.success(userAnswerMapper.doAppAnswerCount());
+    }
+
+    /**
+     * 某应用回答结果分布统计
+     *
+     * @param appId
+     * @return
+     */
+    @GetMapping("/answer_result_count")
+    public BaseResponse<List<AppAnswerResultCountDTO>> getAppAnswerResultCount(Long appId) {
+        ThrowUtils.throwIf(appId == null || appId <= 0, ErrorCode.PARAMS_ERROR);
+        return ResultUtils.success(userAnswerMapper.doAppAnswerResultCount(appId));
+    }
+}
--- src/main/java/com/qbw/AIanswer/mapper/UserAnswerMapper.java
+++ src/main/java/com/qbw/AIanswer/mapper/UserAnswerMapper.java
@@ -1,7 +1,12 @@
 package com.qbw.AIanswer.mapper;
 
+import com.qbw.AIanswer.model.dto.statistic.AppAnswerCountDTO;
+import com.qbw.AIanswer.model.dto.statistic.AppAnswerResultCountDTO;
 import com.qbw.AIanswer.model.entity.UserAnswer;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
 * @author 10362
@@ -11,6 +16,15 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 */
 public interface UserAnswerMapper extends BaseMapper<UserAnswer> {
 
+    @Select("select appId, count(userId) as answerCount from user_answer\n" +
+            "    group by appId order by answerCount desc limit 10;")
+    List<AppAnswerCountDTO> doAppAnswerCount();
+
+
+    @Select("select resultName, count(resultName) as resultCount from user_answer\n" +
+            "    where appId = #{appId}\n" +
+            "    group by resultName order by resultCount desc;")
+    List<AppAnswerResultCountDTO> doAppAnswerResultCount(Long appId);
 }
 
 
